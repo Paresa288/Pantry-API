5163b431512bbb11e83d126716778a062865f817
Paresa288 <prehacker99@gmail.com>
2025-10-14 10:38:05 +0200

    Initial Commit

---
+++ Persistence/Entities/Users/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistence.Entities.Roles;
+
+namespace Persistence.Entities.User
+{
+    internal class User
+    {
+        public int ID { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+        
+        [MaxLength(100)]
+        public string Email {  get; set; }
+        public string PasswordHash { get; set; }
+        public int RoleId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
